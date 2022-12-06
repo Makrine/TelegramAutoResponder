12f7bfb815242b5322e1d21cc9e379ef474e8b19
Makrina Tskipurishvili <66353680+Makrine@users.noreply.github.com>
2022-12-07 00:35:49 +0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ from telegrambot_manager import *
 
 
 def main():
-    print(time.asctime(), '-', 'Started!')
+    print(time.asctime() + '-' + 'Started!')
     client.start()
     client.run_until_disconnected()
-    print(time.asctime(), '-', 'Stopped!')
+    print(time.asctime() + '-' +'Stopped!')
     
 if __name__ == '__main__':
     main()
